ee724c26f31501e6460a6483747fde43ec25488d
Carlos Cancines <Carlozc90@gmail.com>
2022-03-11 00:08:10 -0500

    Inicio

---
+++ src/pages/LoginForm.jsx
@@ -0,0 +1,9 @@
+const LoginForm = () => {
+  return (
+    <div>
+      <h1>Formulario</h1>
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ src/pages/NuevoCliente.jsx
@@ -0,0 +1,9 @@
+const NuevoCliente = () => {
+  return (
+    <div>
+      <h1>NuevoCLiente</h1>
+    </div>
+  );
+};
+
+export default NuevoCliente;
